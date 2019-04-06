17b550a7eb447aef075162812df400cd9cbb6a46
Dakota Clark <dakotaclark92@yahoo.com>
2019-04-06 02:26:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+     set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do
-    erb :super_hero
-  end
-  
-  post '/teams' do
+
+    get '/' do
+      erb :super_hero
+    end
+
+
+    post '/teams' do
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
 
@@ -19,6 +21,6 @@ class App < Sinatra::Base
 
       erb :team
     end
-  end
-
+end
+end
 
