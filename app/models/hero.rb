86e346422d1227f9c5e6cf62ad8f4cdcb822460c
Dakota Clark <dakotaclark92@yahoo.com>
2019-04-06 01:50:56 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,37 @@
 class hero
+  attr_reader :name, :power, :biography
+  
+  HEROS = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @biography = params[:biography]
+    HEROS << self
+  end
+  
+  def self.all
+    HEROS
+  end
   
   
+end
+
+
+class Pirate
+  attr_reader :name, :weight, :height
+  
+  PIRATES = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    PIRATES << self
+  end
+  
+  def self.all
+    PIRATES
+  end
   
 end
